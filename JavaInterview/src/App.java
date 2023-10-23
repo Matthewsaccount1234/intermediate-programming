2fcab1b72887f7c559194ad87388a913dcf457b7
Matthew Fratepietro <matthew_fratepietro@horacemann.org>
2023-10-23 17:51:58 -0400

    putting interview onto github

---
+++ JavaInterview/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+
+    
+    public static void main(String[] args) throws Exception {
+        Scanner name_input = new Scanner(System.in);
+        System.out.println("What is your name");
+        String name = name_input.nextLine();
+        System.out.println("Hello " + name);
+        
+        Scanner ice_input = new Scanner(System.in);
+        System.out.println("What is your favorite flavor of ice cream");
+        String ice_cream = ice_input.nextLine();
+        if (ice_cream.length() > 7) {
+            System.out.println("I like " + ice_cream + " too");
+        }
+        else {
+            System.out.println( "I don't like " + ice_cream);
+        }
+       
+
+    }
+}
