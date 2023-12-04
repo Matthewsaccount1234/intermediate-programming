bef760fdf2dc4ad670a172df9fdb7947761a71e2
Matthew Fratepietro <matthew_fratepietro@horacemann.org>
2023-12-03 20:31:10 -0500

    Adding array list to number guess game and fixing bug when 0 is the answer

--- MovieDownload/src/App.java
+++ MovieDownload/src/App.java
@@ -1,27 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class App {
     public static void main(String[] args) throws Exception {
         // 1. Create a new MoviePosterWriter
         MoviePosterWriter mpw = new MoviePosterWriter();
-        String [] movies = {"Avengers", "Jaws", "Star Wars", "Sixth Sense", "A Christmas Story"};
         // 2. Create a new Movie, pass it in a string of a movie title
-        
-        // 3. use the movie poster writer to set a movie and then write it
 
-        for (int loopCounter = 0; loopCounter < movies.length; loopCounter++) {
-            Movie movie = new Movie(movies[loopCounter]);
-            mpw.setMovieString(movie.getMovieNameForURL());
-            mpw.write(movie.getMovieFilename() + loopCounter + ".jpg");
+        // Choose your own movies below and put them into this array! make a loop to
+        // repeat steps 2 and 3.
+        String[] movies = { "avengers", "inception" }; // initialize array
+        String[] movies2 = new String[5]; // 2nd way to initialize array
+        ArrayList<String> movies3 = new ArrayList(); // array list - do not specify size
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < movies.length; i++) {
+            // TODO: change this for loop to a while loop
+            // and run until someone types in STOP
+            System.out.println("Tell me a movie:");
+            movies3.add(sc.nextLine()); // adding
+            // Movie avengers = new Movie(movies[i]); // getting for an array
+            Movie avengers = new Movie(movies3.get(i)); // getting for an arraylist
+            // 3. use the movie poster writer to set a movie and then write it
+            mpw.setMovieString(avengers.getMovieNameForURL());
+            mpw.write(avengers.getMovieFilename());
         }
 
+        System.out.println("We have gotten all of these posters:");
+        // TODO: Print out all movie names
+        for (int i = 0; i < movies3.size(); i++) {
+            System.out.println(movies3.get(i));
+        }
+        movies3.remove(0);
+        System.out.println("Removed one movie");
+        for (int i = 0; i < movies3.size(); i++) {
+            System.out.println(movies3.get(i));
+        }
 
-        // Choose your own movies here! make a loop to repeat steps 2 and 3.
-        // String movies = {"avengers"};
-
-        // this code doesn't work if you have a space in the movie name. see below
-        // Movie airbud = new Movie("air bud");
-        // mpw.setMovieString("air bud");
-        // mpw.write("air bud");
+        // REMOVE ALL THE MOVIES FROM movies3
 
     }
 }
--- NumberGuessGame/src/App.java
+++ NumberGuessGame/src/App.java
@@ -1,33 +1,40 @@
+
 //Matthew Fratepietro
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
-    // Declaring fields for the range of the number, creating the number, and the number of guesses
-    static int range_min = 0;
-    static int range_max = 50;
+    // Declaring fields for the range of the number, creating the number, and the
+    // number of guesses
     static int guesses = 0;
+    static int range_min =0 ;
+    static int range_max = 50;
     static Random rand = new Random();
     static int answer = rand.nextInt(range_min, range_max);
+    static ArrayList<Integer> alreadyGuessed = new ArrayList<Integer>();
 
     public static void main(String[] args) throws Exception {
-        //Begins program
+        // Begins program
+        
         eval_guess(get_new_guess());
 
     }
 
     public static int get_new_guess() {
         System.out.println("Guess a number from " + range_min + " to " + range_max);
-        //creates new scanner
+        // creates new scanner
         Scanner sc = new Scanner(System.in);
-        //checks if the input is an integer
+        // checks if the input is an integer
         if (sc.hasNextInt()) {
-            //if input is an integer, it transfers the input to an integer and returns that value
+            // if input is an integer, it transfers the input to an integer and returns that
+            // value
             String next_guess = sc.next();
             int guess_int = Integer.valueOf(next_guess);
             return guess_int;
         } else {
-            //if input is not an integer, it recurses the function so that you can guess again with no penalty to the number of guesses
+            // if input is not an integer, it recurses the function so that you can guess
+            // again with no penalty to the number of guesses
             System.out.println("This is not an integer. Guess again.");
             get_new_guess();
             return 0;
@@ -35,25 +42,33 @@ public class App {
     }
 
     public static void eval_guess(int guess) {
-
-        if (guess > answer) {
-            //if the guess is higher than the answer, it tells the user to guess lower
-            System.out.println("Lower. What is your next guess?");
-            //adds a guess to guess counter
-            guesses += 1;
-            //reruns method to test next guess
-            eval_guess(get_new_guess());
-        } else if (guess < answer) {
-            //if the guess is lower than the answer, it tells the user to guess higher
-            System.out.println("Higher. What is your next guess?");
-            //adds a guess to guess counter
-            guesses += 1;
-            //reruns method to test next guess
+        if (alreadyGuessed.contains(guess)) {
+            System.out.println("You already guessed that");
             eval_guess(get_new_guess());
         } else {
-            //if the user gets the right answer, game ends and the guesses are printed
-            System.out.println("congratulations you got the answer in " + guesses + " guesses");
-        }
+            if (guess > answer) {
+                // if the guess is higher than the answer, it tells the user to guess lower
+                System.out.println("Lower. What is your next guess?");
+                // adds a guess to guess counter
+                guesses += 1;
+                //adds guess to guessed list
+                alreadyGuessed.add(guess);
+                // reruns method to test next guess
+                eval_guess(get_new_guess());
+            } else if (guess < answer) {
+                // if the guess is lower than the answer, it tells the user to guess higher
+                System.out.println("Higher. What is your next guess?");
+                // adds a guess to guess counter
+                guesses += 1;
+                //adds guess to guessed list
+                alreadyGuessed.add(guess);
+                // reruns method to test next guess
+                eval_guess(get_new_guess());
+            } else {
+                // if the user gets the right answer, game ends and the guesses are printed
+                System.out.println("congratulations you got the answer in " + guesses + " guesses");
+            }
 
+        }
     }
 }
