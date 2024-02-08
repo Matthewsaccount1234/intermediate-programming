ffd478ec6928f9bed9c7ee6b188ca79233962fc7
Matthew Fratepietro <matthew_fratepietro@horacemann.org>
2024-02-08 14:24:29 -0500

    Added high scores to the game

--- NumberGuessGame/src/App.java
+++ NumberGuessGame/src/App.java
@@ -3,21 +3,25 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class App {
     // Declaring fields for the range of the number, creating the number, and the
     // number of guesses
-    static int guesses = 0;
-    static int range_min =0 ;
+    static int guesses = 1;
+    static int range_min = 0;
     static int range_max = 50;
     static Random rand = new Random();
     static int answer = rand.nextInt(range_min, range_max);
     static ArrayList<Integer> alreadyGuessed = new ArrayList<Integer>();
+    static HashMap<String, Integer> highscores = new HashMap<String, Integer>();
+
 
     public static void main(String[] args) throws Exception {
         // Begins program
-        
+    
         eval_guess(get_new_guess());
+        
 
     }
 
@@ -41,6 +45,29 @@ public class App {
         }
     }
 
+
+    public static String name() {
+        System.out.println("What was your name");
+        // creates new scanner
+        Scanner hs = new Scanner(System.in);
+        // checks if the input is an integer
+        String name = hs.next();
+        return name;
+    }
+
+    public static Boolean playAgain() {
+        System.out.println("Type 1 to exit, Type 2 to play again");
+        // creates new scanner
+        Scanner ag = new Scanner(System.in);
+        // checks if the input is an integer
+        String answer = ag.next();
+        if (answer.equals("1")){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     public static void eval_guess(int guess) {
         if (alreadyGuessed.contains(guess)) {
             System.out.println("You already guessed that");
@@ -51,7 +78,7 @@ public class App {
                 System.out.println("Lower. What is your next guess?");
                 // adds a guess to guess counter
                 guesses += 1;
-                //adds guess to guessed list
+                // adds guess to guessed list
                 alreadyGuessed.add(guess);
                 // reruns method to test next guess
                 eval_guess(get_new_guess());
@@ -60,15 +87,32 @@ public class App {
                 System.out.println("Higher. What is your next guess?");
                 // adds a guess to guess counter
                 guesses += 1;
-                //adds guess to guessed list
+                // adds guess to guessed list
                 alreadyGuessed.add(guess);
                 // reruns method to test next guess
                 eval_guess(get_new_guess());
             } else {
                 // if the user gets the right answer, game ends and the guesses are printed
                 System.out.println("congratulations you got the answer in " + guesses + " guesses");
+                set_highscore(name(), guesses);
+                // replay the game
+                if (playAgain()){
+                    answer = rand.nextInt(range_min, range_max);
+                    alreadyGuessed.clear();
+                    guesses = 0;
+                    System.out.println("starting new game");
+                    eval_guess(get_new_guess());
+                } else {
+                    System.out.println("Your highscores are:" + highscores);
+                }
+
             }
 
         }
+    }
+
+    public static void set_highscore(String name, int score) {
+        highscores.put(name, score);
+
     }
 }
